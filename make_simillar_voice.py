e0d3a6eca08447a9a16084a6bd32ac8d70d0539f
etsuura <etsuura@gmail.com>
2020-08-01 21:06:59 +0900

    add noise function add resampling function

--- make_simillar_voice.py
+++ make_simillar_voice.py
@@ -6,14 +6,29 @@ import sox
 import utils.path_utils as pu
 import utils.SignalProcessingTools as spt
 
-def add_noise(data, noise, para):
-    #Todo change shape
-    return data + noise * para
+def noise_cut(data, noise):
+    assert data.shape[0] < noise.shape[0], "noise size is short."
+    if data.shape[0] == noise.shape[0]:
+        return
+    else:
+        cut_noise = np.zeros_like(data)
+        cut_noise = noise[:data.shape[0]]
+    return cut_noise
 
-def sox_resampling(input_path, output_path, SAMPLINGRATE=48000):
-    transformer = sox.Transformer()
-    transformer.rate(samplerate=SAMPLINGRATE)
-    transformer.build(input_path, output_path)
+def add_noise(data, noise, amplitude=1.0):
+    cut_noise = noise_cut(data, noise)
+    return data + cut_noise * amplitude
+
+def sox_resampling(input_path, SAMPLINGRATE=48000):
+    name = os.path.basename(input_path)
+    output_path = os.path.join("./output", name)
+    if not (os.path.isfile(output_path)):
+        transformer = sox.Transformer()
+        transformer.rate(samplerate=SAMPLINGRATE)
+        transformer.build(input_path, output_path)
+    data, fs = spt.read_data(output_path)
+    assert fs == SAMPLINGRATE
+    return data
 
 def sox_timestretch(input_path, name, FACTOR=1.0):
     assert FACTOR != 1.0, "please set FACTOR"
@@ -48,6 +63,10 @@ def sox_lpf(input_path, name, frequency = None):
     data, fs = spt.read_data(output_path)
     return data
 
+def lpf():
+    pass
+
+
 def make_similer_voice(signal_path, noise_path):
     assert os.path.isfile(signal_path)
     assert os.path.isfile(noise_path)
@@ -59,18 +78,11 @@ def make_similer_voice(signal_path, noise_path):
 
     data, fs = spt.read_data(signal_path)
     noise_data, fs_n = spt.read_data(noise_path)
-
     if fs != fs_n:
-        name = os.path.basename(noise_path)
-        noise_path = os.path.join("./output", name)
-        if not(os.path.isfile(noise_path)):
-            sox_resampling(noise_path, noise_path)
-        noise_data, fs_n = spt.read_data(noise_path)
-    assert fs == fs_n
+        noise_data = sox_resampling(noise_path, fs)
 
-    add_noise(data, noise_data, 0.3)
-
-    # Todo LPF
+    add_signal = add_noise(data, noise_data, 0.3)
+    # add_signal = lpf(add_signal, )
 
     pass
 
